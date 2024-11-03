eac3affaf51dbb337c8ce67210d969768d7597e5
mmtg445 <mmtg445@gmail.com>
2024-11-03 14:00:08 +0600

    Update config.py

--- VCsMusicBot/config.py
+++ VCsMusicBot/config.py
@@ -8,18 +8,18 @@ if os.path.exists("local.env"):
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME")
+BOT_TOKEN = getenv("8134572256:AAGyLugyrSRogOyB5mjdTrtJ-GulWk93S8A")
+BOT_NAME = getenv("RM_Music_Finder")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "tgbotproject")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/cf19dda907391656338eb.png")
 admins = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME")
+BOT_USERNAME = getenv("RM_Music_RBot")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "VCsMusicPlayer")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "zauteschat")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "RM_Support_Group")
 PROJECT_NAME = getenv("PROJECT_NAME", "VCsMusicBot")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/ZauteKm/VCsMusicBot")
+SOURCE_CODE = getenv("SOURCE_CODE", "https://t.me/Rahat146Tm")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
